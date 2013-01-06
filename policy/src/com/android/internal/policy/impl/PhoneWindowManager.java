8c4ee4570abb4ebacdf91bc0d069eb8dcbfec5b7
jt1134 <jt1134@gmail.com>
2013-01-06 02:14:55 -0600

    bugfix: allow recent apps dialog to work with menu long-press
    With the original patch, using the recent app dialog from a
    menu button long-press would cause the dialog to be preloaded
    and then dismissed on key-up. Check long-press behavior settings
    as well as short-press to prevent the dismissal.
    
    Change-Id: Idf1d27aa725547e0ddc44905e27da6c1b4540fab

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2303,7 +2303,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     }
                 }
             } else {
-                if (mRecentAppsPreloaded && mPressOnMenuBehavior != KEY_ACTION_APP_SWITCH) {
+                if (mRecentAppsPreloaded && mPressOnMenuBehavior != KEY_ACTION_APP_SWITCH &&
+                        mLongPressOnMenuBehavior != KEY_ACTION_APP_SWITCH) {
                     cancelPreloadRecentApps();
                 }
                 if (mMenuDoCustomAction) {
