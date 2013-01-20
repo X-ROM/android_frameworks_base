e2e950aa738ca26d4289d42c5ea4d1ea12fc2f67
0xD34D <clark@scheffsblend.com>
2013-01-19 17:04:24 -0700

    Create PacBusyDialog to be used for boot message dialog and power dialog PS 1:initial commit with pac graphics PS 2:make it icecold PS 3: updated progress bar + graphic again PS 4: finally we have otpimizing apps 1 of xxx dialogue :D
    PS 5: update graphics for AICP KITKAT
    PS 6: align images
    
    Change-Id: I0188ab0ff2cbfbcf5c4321e5d562d0f5d753447d
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	services/java/com/android/server/power/ShutdownThread.java
    
    Change-Id: I9e8a68178a20aa6a68e302e8eb8ec2939923f33d
    
    Change-Id: I9e8a68178a20aa6a68e302e8eb8ec2939923f33d
    
    Conflicts:
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	services/java/com/android/server/power/ShutdownThread.java

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -21,6 +21,7 @@ package com.android.server.power;
 
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
+import android.app.PacBusyDialog;
 import android.app.Dialog;
 import android.app.IActivityManager;
 import android.app.KeyguardManager;
@@ -291,7 +292,7 @@ public final class ShutdownThread extends Thread {
 
         // throw up an indeterminate system dialog to indicate radio is
         // shutting down.
-        ProgressDialog pd = new ProgressDialog(context);
+        PacBusyDialog pd = new PacBusyDialog(context, android.R.style.Theme_Translucent_NoTitleBar);
         if (mReboot) {
             pd.setTitle(context.getText(com.android.internal.R.string.reboot_system));
             pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
@@ -299,9 +300,13 @@ public final class ShutdownThread extends Thread {
             pd.setTitle(context.getText(com.android.internal.R.string.power_off));
             pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
         }
-        pd.setIndeterminate(true);
-        pd.setCancelable(false);
+        //pd.setIndeterminate(true);
         pd.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+	pd.getWindow().addFlags(
+                 WindowManager.LayoutParams.FLAG_DIM_BEHIND
+                 | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        pd.getWindow().setDimAmount(1);
+        pd.setCancelable(false);
 
         pd.show();
 
