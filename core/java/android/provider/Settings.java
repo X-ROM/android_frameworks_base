0444c4349d39bd3b1243db3c8352ac5c0d9d8bb9
Elliott Hughes <enh@google.com>
2013-08-02 10:00:44 -0700

    If frameworks wants ASCII casing, it should explicity ask for it.
    http://elliotth.blogspot.com/2012/01/beware-convenience-methods.html
    
    Bug: https://code.google.com/p/android/issues/detail?id=58359
    Change-Id: Iaab02e718a7be7bda22e626dca05d79bfd2a8fc4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -59,6 +59,7 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 
 /**
  * The Settings provider contains global system-level device preferences.
@@ -6999,7 +7000,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothHeadsetPriorityKey(String address) {
-            return BLUETOOTH_HEADSET_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_HEADSET_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
@@ -7007,7 +7008,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothA2dpSinkPriorityKey(String address) {
-            return BLUETOOTH_A2DP_SINK_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_A2DP_SINK_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
@@ -7015,7 +7016,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothInputDevicePriorityKey(String address) {
-            return BLUETOOTH_INPUT_DEVICE_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_INPUT_DEVICE_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
