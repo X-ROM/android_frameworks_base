f31a320770fad73797d08d6ea6b116d4f4bde906
winner00 <toverturf@gmail.com>
2013-08-03 23:57:58 -0600

    Toggle stuff. Change-Id: I27c7633565ebc20e04a8d65a5fc9f08cea148886

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/CRoMToggle.java
@@ -0,0 +1,29 @@
+package com.android.systemui.statusbar.toggles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.android.systemui.R;
+
+public class CRoMToggle extends BaseToggle {
+
+    @Override
+    public void init(Context c, int style) {
+        super.init(c, style);
+        setIcon(R.drawable.ic_qs_crom);
+        setLabel(R.string.quick_settings_crom_label);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setClassName("com.crom.settings", "com.crom.settings.Settings$CrSettingsActivity");
+        intent.addCategory("android.intent.category.LAUNCHER");
+        collapseStatusBar();
+        dismissKeyguard();
+        startActivity(intent);
+    }
+
+}
