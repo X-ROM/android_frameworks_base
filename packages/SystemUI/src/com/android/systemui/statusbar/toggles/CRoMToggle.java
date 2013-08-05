9847b63d1420c68dd0fbaf1b0da9b9a54af842db
aways <aways@outlook.com>
2013-08-05 19:06:23 +0200

    Revert "Fix C-Tool Toggle"
    This reverts commit c32e60fc5dd53e9513b62881070aabdc2189c926.

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/CRoMToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/CRoMToggle.java
@@ -12,29 +12,18 @@ public class CRoMToggle extends BaseToggle {
     @Override
     public void init(Context c, int style) {
         super.init(c, style);
-               setIcon(R.drawable.ic_qs_crom);
+        setIcon(R.drawable.ic_qs_crom);
         setLabel(R.string.quick_settings_crom_label);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.setClassName("com.crom.settings", "com.crom.settings$CrSettingsActivity");
+        intent.addCategory("android.intent.category.LAUNCHER");
         collapseStatusBar();
         dismissKeyguard();
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        Intent intent = new Intent("android.intent.action.MAIN");
-        intent.setComponent(ComponentName
-                .unflattenFromString("com.crom.settings/.CrSettingsActivity"));
-        intent.addCategory("android.intent.category.LAUNCHER");
-
-        startActivity(intent);
-        return super.onLongClick(v);
     }
 
 }
