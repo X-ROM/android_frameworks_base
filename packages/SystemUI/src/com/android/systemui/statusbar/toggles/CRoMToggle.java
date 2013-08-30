94f408887c0ead3d417ca626bc008fa824af8aff
aways <aways@outlook.com>
2013-08-30 21:55:29 +0200

    Revert "FW: Open C-RoM settings as a floating window"
    This reverts commit 24656011bebf7b49e1c240d4ccea1613b857ed4c.

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/CRoMToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/CRoMToggle.java
@@ -19,26 +19,13 @@ public class CRoMToggle extends BaseToggle {
 
     @Override
     public void onClick(View v) {
-        try {
-            Intent intent = new Intent("android.intent.action.MAIN");
-            intent.setClassName("com.android.settings", "com.android.settings.Settings$CromSettingsActivity");
-            intent.addFlags(Intent.FLAG_FLOATING_WINDOW);
-            startActivity(intent);
-        } catch(NullPointerException e) {
-            // No intent found for activity component
-        }
-        collapseStatusBar();
-    }
-    @Override
-    public boolean onLongClick(View v) {
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.setComponent(ComponentName
                 .unflattenFromString("com.crom.settings/.CrSettingsActivity"));
         intent.addCategory("android.intent.category.LAUNCHER");
         collapseStatusBar();
+        dismissKeyguard();
         startActivity(intent);
-        collapseStatusBar();
-        return super.onLongClick(v);
     }
 
 }
