074e0937152bcb8ed80ce83e2c9515c4255f1293
Danesh Mondegarian <daneshm90@gmail.com>
2013-09-03 19:54:36 -0700

    Revert "framework: Privacy Guard"
    Change-Id: I82ab85092e54c577a0817f3bedc85063c9a3054a
    
    Conflicts:
    	core/java/android/app/ActivityManagerNative.java
    	core/java/android/content/ContextWrapper.java
    	core/java/android/provider/Settings.java
    	core/res/AndroidManifest.xml
    	core/res/res/drawable-hdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-mdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-xhdpi/stat_notify_privacy_guard.png
    	core/res/res/drawable-xxhdpi/stat_notify_privacy_guard.png
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/am/ActivityManagerService.java
    	services/java/com/android/server/am/BroadcastQueue.java
    	services/java/com/android/server/pm/PackageManagerService.java
    	services/java/com/android/server/pm/PackageSettingBase.java
    	test-runner/src/android/test/mock/MockContext.java
    	test-runner/src/android/test/mock/MockPackageManager.java

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -1286,7 +1286,6 @@ final class ActivityStack {
         } else {
             next.cpuTimeAtResume = 0; // Couldn't get the cpu time of process
         }
-        updatePrivacyGuardNotificationLocked(next);
     }
 
     /**
@@ -1780,7 +1779,7 @@ final class ActivityStack {
                 next.app.pendingUiClean = true;
                 next.app.thread.scheduleResumeActivity(next.appToken,
                         mService.isNextTransitionForward());
-
+                
                 checkReadyForSleepLocked();
 
             } catch (Exception e) {
@@ -1842,34 +1841,6 @@ final class ActivityStack {
         }
 
         return true;
-    }
-
-    private final void updatePrivacyGuardNotificationLocked(ActivityRecord next) {
-
-        if (mPrivacyGuardPackageName != null && mPrivacyGuardPackageName.equals(next.packageName)) {
-            return;
-        }
-
-        boolean privacy = false;
-
-        try {
-            privacy = AppGlobals.getPackageManager().getPrivacyGuardSetting(
-                    next.packageName, next.userId);
-        } catch (RemoteException e) {
-            // nothing
-        }
-
-        if (mPrivacyGuardPackageName != null && !privacy) {
-            Message msg = mService.mHandler.obtainMessage(
-                    ActivityManagerService.CANCEL_PRIVACY_NOTIFICATION_MSG, next.userId);
-            msg.sendToTarget();
-            mPrivacyGuardPackageName = null;
-        } else if (privacy) {
-            Message msg = mService.mHandler.obtainMessage(
-                    ActivityManagerService.POST_PRIVACY_NOTIFICATION_MSG, next);
-            msg.sendToTarget();
-            mPrivacyGuardPackageName = next.packageName;
-        }
     }
 
     private final void startActivityLocked(ActivityRecord r, boolean newTask,
