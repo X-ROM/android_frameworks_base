b25ab7af88d501dcc7457a3d70b7342500455c09
aways <aways@outlook.com>
2013-09-12 19:51:00 +0200

    Fix Merge Derp

--- services/java/com/android/server/wm/AppTransition.java
+++ services/java/com/android/server/wm/AppTransition.java
@@ -61,7 +61,7 @@ import static com.android.internal.R.styleable.WindowAnimation_wallpaperIntraOpe
 import static com.android.internal.R.styleable.WindowAnimation_wallpaperIntraCloseEnterAnimation;
 import static com.android.internal.R.styleable.WindowAnimation_wallpaperIntraCloseExitAnimation;
 
-import com.android.internal.util.carbon.AwesomeAnimationHelper;
+import com.android.internal.util.crom.AwesomeAnimationHelper;
 import android.widget.Toast;
 
 // State management of app transitions.  When we are preparing for a
