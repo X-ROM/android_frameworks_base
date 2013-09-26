683df30eae70497945a6588490404bc8e5005bf4
Schubi <schubi@erlangen.ccc.de>
2013-09-26 15:43:37 +0200

    Active display: Don't register proximity if pocket mode not used [log]
    Change-Id: I6f27bd86e2dbe2bc14b88acb94668a593590853a

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -752,8 +752,11 @@ public class ActiveDisplayView extends FrameLayout {
     }
 
     private void registerSensorListener() {
-        if (mProximitySensor != null)
+        if (mProximitySensor != null && mPocketModeEnabled) {
             mSensorManager.registerListener(mSensorListener, mProximitySensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            unregisterSensorListener();
+        }
     }
 
     private void unregisterSensorListener() {
