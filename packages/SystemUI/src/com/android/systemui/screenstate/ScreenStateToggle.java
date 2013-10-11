30806b1917811fa3571f15e33e7a07e3958e96c4
aways <aways@c-rom.biz>
2013-10-11 02:58:46 +0200

    Revert "suspend actions: config toggle to run in thread or not"
    This reverts commit 69eaf56d9ecdf0b3141b722be9d6446f8ae4c508.

--- packages/SystemUI/src/com/android/systemui/screenstate/ScreenStateToggle.java
+++ packages/SystemUI/src/com/android/systemui/screenstate/ScreenStateToggle.java
@@ -18,21 +18,19 @@ package com.android.systemui.screenstate;
 import android.content.Context;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
-import android.os.Looper;
+import android.os.Handler;
 
 public abstract class ScreenStateToggle {
     protected Context mContext;
     protected boolean mDoAction = false;
     protected ScreenStateService mService;
+    private Handler mHandler;
 
     public ScreenStateToggle(Context context){
         mContext = context;
+        mHandler = new Handler();
     }
 
-    protected boolean runInThread(){
-        return true;
-    }
-    
     protected abstract boolean isEnabled();
     protected abstract boolean doScreenOnAction();
     protected abstract boolean doScreenOffAction();
@@ -42,38 +40,30 @@ public abstract class ScreenStateToggle {
     public void doScreenOff(){
         if(isEnabled() && doScreenOffAction()){
             final Runnable r = getScreenOffAction();
-            if(runInThread()){
-                Thread thread = new Thread()
-                {
-                    @Override
-                    public void run() {
-                        r.run();
-                    }
-                };
+            Thread thread = new Thread()
+            {
+                @Override
+                public void run() {
+                    mHandler.post(r);
+                }
+            };
 
-                thread.start();
-            } else {
-                r.run();
-            }
+            thread.start();
         }
 
     }
     public void doScreenOn(){
         if(isEnabled() && doScreenOnAction()){
             final Runnable r = getScreenOnAction();
-            if(runInThread()){
-                Thread thread = new Thread()
-                {
-                    @Override
-                    public void run() {
-                        r.run();
-                    }
-                };
+            Thread thread = new Thread()
+            {
+                @Override
+                public void run() {
+                    mHandler.post(r);
+                }
+            };
 
-                thread.start();
-            } else {
-                r.run();
-            }
+            thread.start();
         }
     }
 }
