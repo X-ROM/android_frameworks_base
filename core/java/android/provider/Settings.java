0cd3f560d4f07402f3cee21b99f9b584cab02ce7
winner00 <toverturf@gmail.com>
2013-10-12 20:26:49 -0600

    Add back Thinking Bridge traffic meter
    PS3: Make color use STATUSBAR_NETWORK_COLOR
    
    Change-Id: If6101026b1967528eaf1b8ae2b5172d535bb9439

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2996,6 +2996,12 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_BATTERY = "status_bar_battery";
+
+        /**
+         * Network speed indicator
+          * @hide
+          */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
 		
         /**
          * Whether to show the network status in the status bar
