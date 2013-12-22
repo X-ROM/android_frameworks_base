a9a55a28e500c5ad1a3014e0842efd25657e5998
MURA Stefano <ssspinni@gmail.com>
2013-12-22 12:47:38 +0100

    Network speed indicator on status bar (1/2) Thanks to htchoi1995 && mithun46 Mine rearranged, modified and adapted.
    Conflicts:
    	packages/SystemUI/res/values/colors.xml
    
    Change-Id: Ia4397d132f2ee6cbb72028f7d94d04d96ab6c394

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2515,7 +2515,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         @Override
         public void run() {
             checkBarModes();
-        }};
+        }
+    };
 
     @Override
     public void setInteracting(int barWindow, boolean interacting) {
