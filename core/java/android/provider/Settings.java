64d487e206adc3df28e4f247d259076c2cc8c362
Sasikumar D R <sasikumardr@gmail.com>
2014-01-07 02:41:17 +0530

    [1/3] Flip to Mute/Reject Call
    saving flip action value to Setings container
    
    Change-Id: Ie72dda49d78b23b9bc7d8a886f6916f678c918d1
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3699,6 +3699,13 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+         * Should Flip to Silence be used
+         *
+         * @hide
+         */
+        public static final String FLIP_ACTION_KEY = "flip_action";
+
+        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
