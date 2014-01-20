5dd241647d5620a01c438a1a6c3bdc05ebec72be
lion0738 <lion0738@naver.com>
2014-01-20 14:39:44 +0900

    Lockscreen Notifications: Fix NPE
    E/AndroidRuntime( 3778): java.lang.NullPointerException
    E/AndroidRuntime( 3778): 	at com.android.keyguard.NotificationViewManager.onScreenTurnedOn(NotificationViewManager.java:286)
    E/AndroidRuntime( 3778): 	at com.android.keyguard.KeyguardViewManager.onScreenTurnedOn(KeyguardViewManager.java:825)
    E/AndroidRuntime( 3778): 	at com.android.keyguard.KeyguardViewMediator.handleNotifyScreenOn(KeyguardViewMediator.java:1416)
    E/AndroidRuntime( 3778): 	at com.android.keyguard.KeyguardViewMediator.access$2300(KeyguardViewMediator.java:104)
    E/AndroidRuntime( 3778): 	at com.android.keyguard.KeyguardViewMediator$4.handleMessage(KeyguardViewMediator.java:1131)
    E/AndroidRuntime( 3778): 	at android.os.Handler.dispatchMessage(Handler.java:102)
    
    Fix NPE on first boot
    
    Change-Id: I6aa4f62ffbc00be15aa98ee3a137bb0d6add6610

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -821,7 +821,7 @@ public class KeyguardViewManager {
             }
         }
 
-        if (mLockscreenNotifications) {
+        if (mLockscreenNotifications && mKeyguardView != null) {
             mNotificationViewManager.onScreenTurnedOn();
         }
     }
