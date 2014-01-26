7d5083b48d06e9c2acf5277cf19d57516aeb134b
Steve Spear <stephen.k.spear@gmail.com>
2014-01-26 16:02:05 +0100

    FWB: AOKP custom system animations (1/2)
    C-RoM changes for kitkat AOKP branch
    
    Change-Id: Ibfe6d48aa37d1fc9af39122213ec4c823b5cd2ab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3648,6 +3648,26 @@ public final class Settings {
         public static final String CALL_END_SOUND = "call_end_sound";
 
         /**
+         * Custom System Animations
+         *
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        /**
          * MediaScanner behavior on boot.
          * 0 = enabled
          * 1 = ask (notification)
