9782d5343b455be90a47a008396d809be395abf7
aways <aways@c-rom.org>
2014-02-03 02:30:44 +0100

    Remove duplicate lines

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -1594,14 +1594,4 @@ public class GlowPadView extends View {
         }
         mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
     }
-
-    public void setHandleDrawable(Drawable handle) {
-        Resources res = mContext.getResources();
-        if (handle != null) {
-            mHandleDrawable = new TargetDrawable(res, handle);
-        } else {
-            mHandleDrawable = new TargetDrawable(res, 0);
-        }
-        mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
-    }
 }
