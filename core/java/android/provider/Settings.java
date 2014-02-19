fd8dda6241c203d4548f148d8d435cd52ff240d0
aways <aways@c-rom.org>
2014-02-19 13:44:01 +0100

    Revert "FWB: AOKP custom system animations (1/2)"
    This reverts commit 7d5083b48d06e9c2acf5277cf19d57516aeb134b.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4035,26 +4035,6 @@ public final class Settings {
         public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
 
         /**
-         * Custom System Animations
-         *
-         * @hide
-         */
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
-                "activity_open",
-                "activity_close",
-                "task_open",
-                "task_close",
-                "task_to_front",
-                "task_to_back",
-                "wallpaper_open",
-                "wallpaper_close",
-                "wallpaper_intra_open",
-                "wallpaper_intra_close",
-        };
-        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
-
-        /**
          * MediaScanner behavior on boot.
          * 0 = enabled
          * 1 = ask (notification)
