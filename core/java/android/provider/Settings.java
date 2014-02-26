216886f5119a89a4ef2ea036a5857ca50d7d7d9b
aways <aways@c-rom.org>
2014-02-26 01:00:42 +0100

    fix build

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4570,7 +4570,7 @@ public final class Settings {
          * Allows blurring the lockscreen background
          * @hide
          */
-        public static final String LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
+        public static final String LOCKSCREEN_BLUR_BEHIND = "lockscreen_blur_behind";
 
         /**
          * Whether to show the network status in the status bar
