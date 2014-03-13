98349c59be38151ab2d4bdc31c63475c77a9ac4a
aways <aways@c-rom.org>
2014-03-13 11:57:48 +0100

    Remove Halo Icon from statusbar
    Credits:  LorDClockaN

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -172,7 +172,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected boolean mHaloEnabled;
     protected boolean mHaloActive;
     public boolean mHaloTaskerActive = false;
-    protected ImageView mHaloButton;
 
     // Search panel
     protected SearchPanelView mSearchPanelView;
@@ -544,10 +543,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 Settings.System.HALO_ENABLED, 0) == 1;
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ACTIVE, 0) == 1;
-
-        mHaloButton.setImageResource(mHaloActive
-                ? R.drawable.ic_notify_halo_pressed
-                : R.drawable.ic_notify_halo_normal);
 
         if (!mHaloEnabled) {
             mHaloActive = false;
