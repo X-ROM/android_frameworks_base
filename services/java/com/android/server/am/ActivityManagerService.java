9f5be12131330f3ccbf92424052eb85e14b130ba
Akira Numata <akira.numata@sonymobile.com>
2014-03-13 13:36:31 +0900

    Insufficient ProcessRecord cleanup when persistent process is killed
    When persistent process with Service restarts, ActivityManagerService
    does not reset ProcessRecord#hasClientActivites to false
    (because ProcessRecord of persistent process is continued using
    after killing).
    
    It disturbs updating LRU list in ActivityManagerService, and then,
    when new process calls ActivityManagerProxy#publishContentProviders,
    SecurityException happens because of no entry in the list.
    
    Bug: 13517358
    
    Change-Id: I46b064f71a4f7025ade1bf117801352a7ab22e6a

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -12578,6 +12578,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         app.foregroundActivities = false;
         app.hasShownUi = false;
         app.hasAboveClient = false;
+        app.hasClientActivities = false;
 
         mServices.killServicesLocked(app, allowRestart);
 
