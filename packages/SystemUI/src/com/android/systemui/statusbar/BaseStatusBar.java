9a8710adb51f00da413ba201f3f66f3acfab398b
Lars Greiss <kufikugel@googlemail.com>
2014-03-16 12:00:00 +0100

    Frameworks: SlimRecents make it RTL/LTR aware + scaling + lefty mode (1/3)
    This commit adds several features:
    
    - Make it fully RTL/LTR aware. Means it shows up on the oposite side if an RTL
      language was choosen. As well icon, textposition, menu etc follows RTL direction.
      So like AOSP guidelines suggest it is fully compatible now for all locals
    
    - Add scaling mode....user can scale the overall size down (especially for the
      user with stock dpi requested)
    
    - Add lefty mode. Recent panel shows up on the left edge instead of the right edge
    
    As well this commit unlinks the construct of the card with the update of the content. This
    reduces one update call on construct and gives a tight faster loading.
    
    Change-Id: I4c5105f86644a08da81861851da7e0e347c26244

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -398,14 +398,14 @@ public abstract class BaseStatusBar extends SystemUI implements
         mCustomRecent = Settings.System.getBoolean(mContext.getContentResolver(),
                 Settings.System.CUSTOM_RECENT, false);
 
+        mLocale = mContext.getResources().getConfiguration().locale;
+        mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
+
         if(mCustomRecent){
-            cRecents = new RecentController(mContext);
+            cRecents = new RecentController(mContext, mLayoutDirection);
         }else{
             mRecents = getComponent(RecentsComponent.class);
         }
-
-        mLocale = mContext.getResources().getConfiguration().locale;
-        mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
 
         mStatusBarContainer = new FrameLayout(mContext);
 
