41eed8a84e28d4505fb6e0cd1f0867b4fbd68c1a
aways <aways@c-rom.org>
2014-03-19 17:42:43 +0100

    Fix ?

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -11,9 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
     src/com/android/systemui/EventLogTags.logtags
 
 LOCAL_JAVA_LIBRARIES := telephony-common
-LOCAL_STATIC_JAVA_LIBRARIES := android-opt-cards
-
-LOCAL_STATIC_JAVA_LIBRARIES := guava
+LOCAL_STATIC_JAVA_LIBRARIES := guava android-opt-cards
 
 LOCAL_PACKAGE_NAME := SystemUI
 LOCAL_CERTIFICATE := platform
