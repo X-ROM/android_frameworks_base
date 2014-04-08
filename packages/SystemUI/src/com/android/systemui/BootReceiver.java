dc9c70c523de6932afd276f9e1462a62ebd9c369
aways <aways@c-rom.org>
2014-04-08 11:41:54 +0200

    Revert "Reimplement PowerSaverManager !"
    This reverts commit 5acb0051018794f6c039cd1bf98759fc39228d1c.
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/src/com/android/systemui/BootReceiver.java

--- packages/SystemUI/src/com/android/systemui/BootReceiver.java
+++ packages/SystemUI/src/com/android/systemui/BootReceiver.java
@@ -62,12 +62,6 @@ public class BootReceiver extends BroadcastReceiver {
                 context.startService(screenstate);
             }
 
-            // start the power saver service if activated
-            if (Settings.System.getInt(res, Settings.System.POWER_SAVER_ENABLED, 1) != 0) {
-                Intent powersaver = new Intent(context, com.android.systemui.powersaver.PowerSaverService.class);
-                context.startService(powersaver);
-            }
-
         } catch (Exception e) {
             Log.e(TAG, "Can't start load average service", e);
         }
