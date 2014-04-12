fe97be720600d8940bef9bee59209bec0e8d9457
aways <aways@c-rom.org>
2014-04-12 13:46:15 +0200

    Revert "Session: Add a shortcut in detecting if a window that contains the wallpaper is null or not"
    This reverts commit 285adfef70847f61db845e2580a67fd271ff732c.

--- services/java/com/android/server/wm/Session.java
+++ services/java/com/android/server/wm/Session.java
@@ -392,11 +392,9 @@ final class Session extends IWindowSession.Stub
         synchronized(mService.mWindowMap) {
             long ident = Binder.clearCallingIdentity();
             try {
-                if (window != null) {
-                    mService.setWindowWallpaperPositionLocked(
-                            mService.windowForClientLocked(this, window, true),
-                            x, y, xStep, yStep);
-                }
+                mService.setWindowWallpaperPositionLocked(
+                        mService.windowForClientLocked(this, window, true),
+                        x, y, xStep, yStep);
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
