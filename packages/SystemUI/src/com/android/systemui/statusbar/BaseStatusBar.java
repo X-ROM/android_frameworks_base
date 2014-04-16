fc5266700d766c42899743e64ad37e174e490404
lion0738 <lion0738@naver.com>
2014-04-16 21:00:24 +0200

    Floating window: Enable transparent layer
    Halo: Re-enable transparent layer
    
    Change-Id: I3af24ed40314ca5edb1e5157c0ad9df8ac89c99f
    
    App sidebar: enable transparent layer for floating apps
    
    + cleanup
    
    Change-Id: I6ae8209f7d7839c78840e69bc9597d43fbedc790
    
    enable transparent layer on notification long click
    
    Change-Id: I058a19776bd90d964b5d5df4fcf251c784b3ba8c

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -741,7 +741,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                 null, UserHandle.CURRENT);
     }
 
-    private void launchFloating(PendingIntent pIntent) {
+    private void launchFloating(PendingIntent pIntent, String mPkg) {
+        if (!"android".equals(mPkg)) {
+            Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
+            transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+            mContext.startActivity(transparent);
+        }
         Intent overlay = new Intent();
         overlay.addFlags(Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         try {
@@ -775,7 +780,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
                 //Long click menu broken on PIE mode...pop up menu is useless (auto-launch on long click)
                 if (expanded) {
-                    launchFloating(contentIntent);
+                    launchFloating(contentIntent, packageNameF);
                     animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
                     return true;
                 }
@@ -834,7 +839,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                             am.clearApplicationUserData(packageNameF,
                                     new FakeClearUserDataObserver());
                         } else if (item.getItemId() == R.id.notification_floating_item) {
-                            launchFloating(contentIntent);
+                            launchFloating(contentIntent, packageNameF);
                             animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
                         } else if (item.getItemId() == R.id.notification_hide_icon_packages) {
                             item.setChecked(!item.isChecked());
@@ -1294,11 +1299,11 @@ public abstract class BaseStatusBar extends SystemUI implements
             //int flags = Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK;
             if (mPendingIntent != null) {
 
-                 /*if (mFloat && !"android".equals(mPkg)) {
+                 if (mFloat && !"android".equals(mPkg)) {
                     Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
-                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_FLOATING_WINDOW);
+                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
                     mContext.startActivity(transparent);
-                }*/
+                }
 
                 int[] pos = new int[2];
                 v.getLocationOnScreen(pos);
