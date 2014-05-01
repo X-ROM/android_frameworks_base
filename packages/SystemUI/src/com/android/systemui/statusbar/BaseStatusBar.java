36d4d2b604a31daf6b4a5f5c8c3489a019a90f0c
aways <aways@c-rom.org>
2014-05-01 19:32:14 +0200

    Revert "Remove HALO icon"
    This reverts commit c22b96e15341e27192d31e615a81bb1957cb896b.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -173,8 +173,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected Halo mHalo = null;
     protected Ticker mTicker;
     protected boolean mHaloEnabled;
+    protected boolean mHaloButtonVisible = false;
     protected boolean mHaloActive;
     public boolean mHaloTaskerActive = false;
+    protected ImageView mHaloButton;
 
     // Search panel
     protected SearchPanelView mSearchPanelView;
@@ -545,12 +547,26 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateHalo();
     }
 
+    protected void updateHaloButton() {
+        if (!mHaloEnabled) {
+            mHaloButtonVisible = false;
+        } else {
+            mHaloButtonVisible = true;
+        }
+        if (mHaloButton != null) {
+            mHaloButton.setVisibility(mHaloButtonVisible && !mHaloActive ? View.VISIBLE : View.GONE);
+        }
+    }
+
     protected void updateHalo() {
         mHaloEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ENABLED, 0) == 1;
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ACTIVE, 0) == 1;
 
+        mHaloButton.setImageResource(mHaloActive
+                ? R.drawable.ic_notify_halo_pressed
+                : R.drawable.ic_notify_halo_normal);
 
         if (!mHaloEnabled) {
             mHaloActive = false;
