14ea9752484cde22d7f039e94bb2c0845c3e56e8
aways <aways@c-rom.org>
2014-05-04 16:51:38 +0200

    Revert "PEEK: configurable pickup timeout (1/2)"
    This reverts commit e7ee43f8c0064b27bd318696b6198c83c37e8e6d.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5060,12 +5060,6 @@ public final class Settings {
         public static final String PEEK_STATE = "peek_state";
 
         /**
-         * Set the timeout of peek when pikcing up the device
-         * @hide
-         */
-        public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
