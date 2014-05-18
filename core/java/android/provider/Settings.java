0723535678e38782578c5607af8f50601dd4d572
LorDClockaN <davor@losinj.com>
2014-05-18 08:07:48 -0300

    Peek wake timeout (1/2)
    - Originaly taken from AICP - Thath's why i'm keeping original autorship
    - Allow user to set how long peek notification will hold screen on
    - Minor changes by me @cristianomatos to match crDroid sources
    
    Diff from original commit:
    * Variable declared globaly and moved from inside the private void scheduleTasks() method
    * Made multi user
    
    Original commit: https://github.com/AICP/frameworks_base/commit/90b47fc55ec59520d6dd5dd828ff48a3a3dca1d4
    
    Change-Id: I3a3c7e6f99edd9c861566b2d738d31e7657a0322

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5133,6 +5133,13 @@ public final class Settings {
          */
         public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
 
+       /**
+         * Time to show notification
+         * 5000ms = default
+         * @hide
+         */
+        public static final String PEEK_WAKE_TIMEOUT = "peek_wake_timeout";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
